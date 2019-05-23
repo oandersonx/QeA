71b7b399c96bba9cae4ef42001570d23a266cf4f
Anderson <anderson.jribeiro1@gmail.com>
2019-05-23 18:35:48 -0300

    configurando MainActivity

--- app/src/main/java/com/example/qea/MainActivity.java
+++ app/src/main/java/com/example/qea/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.qea;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnEntrar, btnRegistrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnEntrar = (Button)findViewById(R.id.btnEntrar);
+        btnRegistrar = (Button)findViewById(R.id.btnRegistrar);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(MainActivity.this, RegistrarActivity.class);
+                startActivity(i);
+            }
+
+        });
     }
 }
