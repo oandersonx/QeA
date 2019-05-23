2eef3827ca802e6edaefecbf40dbdfb435c38d6a
Anderson <anderson.jribeiro1@gmail.com>
2019-05-23 19:46:21 -0300

    configurando Login Status

--- app/src/main/java/com/example/qea/LoginActivity.java
+++ app/src/main/java/com/example/qea/LoginActivity.java
@@ -2,12 +2,31 @@ package com.example.qea;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText edtUsuario, edtSenha;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edtUsuario = (EditText) findViewById(R.id.edtUsuario);
+        edtSenha = (EditText) findViewById(R.id.edtSenha);
+
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = edtUsuario.getText().toString();
+                String password = edtSenha.getText().toString();
+            }
+        });
     }
 }
