c72a5e2ef9ad9c9094a69c046cd155f433ea70e4
Anderson <anderson.jribeiro1@gmail.com>
2019-05-23 18:32:13 -0300

    class DBHelper

---
+++ app/src/main/java/com/example/qea/DBHelper.java
@@ -0,0 +1,49 @@
+package com.example.qea;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static int versao = 1;
+    private static String nome = "PDS2.db";
+
+
+    public DBHelper(Context context) {
+        super(context,nome, null, versao);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String str = "CREATE TABLE Utilizador (username TEXT PRIMARY KEY , password TEXT);";
+        db.execSQL(str);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS Utilizador;");
+        onCreate(db);
+    }
+
+
+    public long CriarUtilizador(String username, String password){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("username", username);
+        cv.put("password", password);
+
+        long result = db.insert("Utilizador", null, cv);
+        return result;
+
+    }
+
+    public String ValidarLogin(String username, String password){
+        return "";
+    }
+}
+
+
+
